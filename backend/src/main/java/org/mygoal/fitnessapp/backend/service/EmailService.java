102ee4491f46f1dcba6f83cc4ee2cd9dc89e7435
Dmitriy Kryzhanovskiy <dmitr.kryzh@gmail.com>
2024-05-05 23:13:13 +0300

    Fix email service with datetime

--- backend/src/main/java/org/mygoal/fitnessapp/backend/service/EmailService.java
+++ backend/src/main/java/org/mygoal/fitnessapp/backend/service/EmailService.java
@@ -10,6 +10,7 @@ import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
+import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 
@@ -21,48 +22,47 @@ public class EmailService {
 
     @Async
     public void sendEmailParams(User user) throws MailException {
-        System.out.println("Prepare to send mail");
+        LocalDateTime moscowTime = LocalDateTime.now(ZoneId.of("Europe/Moscow"));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+        String formattedDate = moscowTime.format(formatter);
         SimpleMailMessage mail = new SimpleMailMessage();
         mail.setFrom(environment.getProperty("spring.mail.username"));
         mail.setTo(user.getEmail());
-        mail.setSubject("Параметры тела (" + DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT)
-                .format(LocalDateTime.now()) + ")");
+        mail.setSubject("Параметры тела (" + formattedDate + ")");
 
-        mail.setText(
-                String.format(
-                        """
-                                Ваши параметры тела на %s:
-
-                                Рост: %.1f см
-
-                                Вес: %.1f кг
-
-                                Жир: %.1f%%
-
-                                Ширина плеч: %.1f см
-
-                                Обхват плеча: %.1f см
-
-                                Обхват груди: %.1f см
-
-                                Обхват талии: %.1f см
-
-                                Обхват бедер: %.1f см
-
-                                Обхват икр: %.1f см
-                              """,
-                        DateTimeFormatter.ofLocalizedDateTime(FormatStyle.SHORT).format(LocalDateTime.now()),
-                        user.getHeight(),
-                        user.getWeight(),
-                        user.getFat(),
-                        user.getShoulderWidth(),
-                        user.getShoulderCircumference(),
-                        user.getChestCircumference(),
-                        user.getWaistCircumference(),
-                        user.getHipCircumference(),
-                        user.getCalfCircumference()
-                )
-        );
+        mail.setText(String.format(
+                """
+                Ваши параметры тела на %s:
+        
+                Рост: %.1f см
+        
+                Вес: %.1f кг
+        
+                Жир: %.1f%%
+        
+                Ширина плеч: %.1f см
+        
+                Обхват плеча: %.1f см
+        
+                Обхват груди: %.1f см
+        
+                Обхват талии: %.1f см
+        
+                Обхват бедер: %.1f см
+        
+                Обхват икр: %.1f см
+                """,
+                formattedDate,
+                user.getHeight(),
+                user.getWeight(),
+                user.getFat(),
+                user.getShoulderWidth(),
+                user.getShoulderCircumference(),
+                user.getChestCircumference(),
+                user.getWaistCircumference(),
+                user.getHipCircumference(),
+                user.getCalfCircumference()
+        ));
 
         emailSender.send(mail);
     }
