57ca1e32a855b386bce22b519aa554806793b17c
Dmitriy Kryzhanovskiy <dmitr.kryzh@gmail.com>
2024-05-07 23:50:27 +0300

    Fix db volume in container

--- backend/src/main/resources/sql/init.sql
+++ backend/src/main/resources/sql/init.sql
@@ -5,20 +5,57 @@ VALUES ('Иван', 'Кузнецов', '4.5'),
 
 
 INSERT INTO public.training (coach_id, user_id, status, training_date_time)
-VALUES (1, null, 'not started', '2024-05-05T14:00:00'),
-       (3, 1, 'finished', '2024-04-25T14:00:00'),
-       (2, 1, 'not started', '2024-05-01T15:00:00'),
-       (1, null, 'not started', '2024-05-01T18:00:00'),
-       (2, null, 'not started', '2024-05-01T19:00:00'),
-       (3, null, 'not started', '2024-05-05T10:00:00'),
-       (2, null, 'not started', '2024-05-05T13:00:00'),
-       (2, null, 'not started', '2024-05-05T18:00:00'),
-       (2, null, 'not started', '2024-05-05T19:00:00'),
-       (2, null, 'not started', '2024-05-05T20:00:00'),
-       (2, null, 'not started', '2024-05-05T09:00:00'),
-       (2, null, 'not started', '2024-05-05T10:00:00'),
-       (1, null, 'not started', '2024-05-05T08:00:00'),
-       (1, null, 'not started', '2024-05-05T14:00:00'),
-       (2, null, 'not started', '2024-05-07T09:00:00'),
-       (2, null, 'not started', '2024-05-07T10:00:00'),
-       (1, null, 'not started', '2024-05-07T08:00:00');
+VALUES (1, 1, 'finished', '2024-05-02T15:30:00'),
+       (1, null, 'not started', '2024-05-08T15:30:00'),
+       (1, null, 'not started', '2024-05-08T16:30:00'),
+       (1, null, 'not started', '2024-05-08T17:30:00'),
+       (1, null, 'not started', '2024-05-08T18:30:00'),
+       (1, null, 'not started', '2024-05-08T19:30:00'),
+
+       (2, null, 'not started', '2024-05-08T09:00:00'),
+       (2, null, 'not started', '2024-05-08T10:00:00'),
+       (2, null, 'not started', '2024-05-08T12:00:00'),
+       (2, null, 'not started', '2024-05-08T13:00:00'),
+       (2, null, 'not started', '2024-05-08T16:00:00'),
+
+       (3, null, 'not started', '2024-05-08T14:00:00'),
+       (3, null, 'not started', '2024-05-08T16:00:00'),
+       (3, null, 'not started', '2024-05-08T17:00:00'),
+       (3, null, 'not started', '2024-05-08T19:00:00'),
+       (3, null, 'not started', '2024-05-08T18:00:00'),
+---------------------------------------------------------------------
+       (2, null, 'not started', '2024-05-09T15:30:00'),
+       (2, null, 'not started', '2024-05-09T17:30:00'),
+       (2, null, 'not started', '2024-05-09T18:30:00'),
+       (2, null, 'not started', '2024-05-09T19:30:00'),
+
+       (3, null, 'not started', '2024-05-09T09:00:00'),
+       (3, null, 'not started', '2024-05-09T11:00:00'),
+       (3, null, 'not started', '2024-05-09T12:00:00'),
+       (3, null, 'not started', '2024-05-09T15:00:00'),
+       (3, null, 'not started', '2024-05-09T16:00:00'),
+
+       (1, null, 'not started', '2024-05-09T14:00:00'),
+       (1, null, 'not started', '2024-05-09T15:00:00'),
+       (1, null, 'not started', '2024-05-09T17:00:00'),
+       (1, null, 'not started', '2024-05-09T20:00:00'),
+       (1, null, 'not started', '2024-05-09T18:00:00'),
+---------------------------------------------------------------------
+
+       (3, null, 'not started', '2024-05-10T15:30:00'),
+       (3, null, 'not started', '2024-05-10T17:30:00'),
+       (3, null, 'not started', '2024-05-10T18:30:00'),
+       (3, null, 'not started', '2024-05-10T19:30:00'),
+
+       (1, null, 'not started', '2024-05-10T09:00:00'),
+       (1, null, 'not started', '2024-05-10T11:00:00'),
+       (1, null, 'not started', '2024-05-10T12:00:00'),
+       (1, null, 'not started', '2024-05-10T15:00:00'),
+       (1, null, 'not started', '2024-05-10T16:00:00'),
+
+       (2, null, 'not started', '2024-05-10T14:00:00'),
+       (2, null, 'not started', '2024-05-10T15:00:00'),
+       (2, null, 'not started', '2024-05-10T19:00:00'),
+       (2, null, 'not started', '2024-05-10T20:00:00'),
+       (2, null, 'not started', '2024-05-10T18:00:00')
+;
